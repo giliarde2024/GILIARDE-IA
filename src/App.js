eaf00f75acb3b8f8b2b938ab24a2a5719edb1bf2
giliarde2024 <giliarde_r.o@hotmail.com>
2025-06-03 00:30:06 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Bem-vindo ao Giliarde-IA</h1>
+      <p>Seu app de geração de vídeos com IA!</p>
+      <p>Se quiser apoiar o projeto:</p>
+      <strong>Chave Pix: wwwgiliardemc@gmail.com</strong>
+    </div>
+  );
+}
+
+export default App;
